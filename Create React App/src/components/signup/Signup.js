502ae3e8826e9af644a6c5faf077b300001a3464
Jakub Kubiš <jkubisg.i@gmail.com>
2016-08-24 23:04:17 +0200

    Static 0-1-1 Screen

--- Create React App/src/components/signup/Signup.js
+++ Create React App/src/components/signup/Signup.js
@@ -12,12 +12,12 @@ class Signup extends Component {
 
           <div className ="Signup-left-content">
             <div className="Signup-left-message>">
-              <h2 className="Sighup-left-message-header">How do you want to call your team?</h2>
+              <h2 className="Signup-left-message-header">How do you want to call your team?</h2>
               <p className="Signup-left-message-p">Your name will be used for communication between you and your teammates.</p>
             </div>
 
             <div className="Signup-left-input">
-              <p className="Signup-left-input-companyname">Company Name</p>
+              <p className="Signup-left-input-p">Company Name</p>
               <input className="Signup-left-input-box" type="text" value="Test" />
             </div>
           </div>
